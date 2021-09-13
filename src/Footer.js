3c940224c384b7bb589de81000a95fab1c849e96
TorchAblaze <tiffanygreathead@gmail.com>
2021-09-12 21:22:17 -0700

    Add footer component

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <footer className="footer">
+        <p>Spacetagram by Shopify 2021</p>
+      </footer>
+    )
+  }
+}
+
+
+export default Footer;
